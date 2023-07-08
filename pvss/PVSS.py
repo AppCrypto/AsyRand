feeadd5b28807ab8a43371c06b5b98af5d51e69e
zhangliang <briliasm@qq.com>
2023-07-08 22:44:43 +0800

    update PVSS interfaces

--- pvss/PVSS.py
+++ pvss/PVSS.py
@@ -6,37 +6,44 @@ from functools import reduce
 import utils.newjson as newjson
 from charm.toolbox.ABEnc import ABEnc, Input, Output
 import random
-import time, sys
-import setting
+import time, sys, os
+# import setting
+import utils.newjson as json
 
 ret = {}
 keyV = 1
 assertExe = True
 
-N = setting.N
-t = setting.t
+# config = json.loads(open("../NodeConfiguration.json","r").read())
+# N = len(config["nodes"])
+# t=int(N/3)+1
+groupObj = PairingGroup("SS512")
 
+def random_scalar():
+    return groupObj.random(ZR)
 
 class PVSS():
-    def random_scalar(self):
-        return self.group.random(ZR)
 
-
-    def __init__(self, groupObj):
-        # global util, group
+    def __init__(self, groupObj, N, t):
         self.util = SecretUtil(groupObj, verbose=False)
         self.group = groupObj
         self.g, self.h = self.group.random(G1), self.group.random(G1)
-        self.sks={i: self.random_scalar() for i in range(0, N+1)}
-        self.pks={i: self.h ** self.sks[i] for i in range(0, N+1)}
-        # print(self.sks)
+        self.pks={}
+        self.N=N
+        self.t=t
+        self.sk=random_scalar()
+        self.pk=self.h ** random_scalar()
+
+    def setPK(self, i, pk):
+        self.pks[i]=pk
+
 
     # PVSS——share
     def share(self, s=None):
         ts = time.time()
 
         w = self.group.random(ZR)        
-        Pis =  self.util.genShares(w, t, N)
+        Pis =  self.util.genShares(w, self.t, self.N)
         if s == None:
             s = self.group.random(ZR)
         
@@ -44,17 +51,17 @@ class PVSS():
         print(self.g ** s)
         # print(self.h ** w)
         C1 = {}
-        for j in range(1, N+1):
+        for j in range(1, self.N+1):
             C1[j] = self.pks[j] ** Pis[j]
             #print(C1[j])
 
         C = {"Com": Com, "C1": C1}        
         _s, _w = self.group.random(ZR), self.group.random(ZR)
         # choose a ploy(x)
-        _pi = self.util.genShares(_w, t, N)
+        _pi = self.util.genShares(_w, self.t, self.N)
         _Com = (self.h ** _w) * (self.g ** _s)
         _C1 = {}
-        for j in range(1, N + 1):
+        for j in range(1, self.N + 1):
             _C1[j] = self.pks[j] ** _pi[j]
         #print(_C1[1])
         Cp = {"_Com": _Com, "_C1": _C1}
@@ -62,7 +69,7 @@ class PVSS():
         stidle = _s - c * s
         wtidle = _w - c * w
         pitidle = {}
-        for i in range(1, N+1):
+        for i in range(1, self.N+1):
             pitidle[i] = _pi[i] - c * Pis[i]
         NIZK_proofs = {"Cp": Cp, "c": c, "stidle": stidle, "wtidle": wtidle, "pitidle": pitidle}
         return {'C': C, 'proof_sw': NIZK_proofs}
@@ -71,11 +78,11 @@ class PVSS():
     def verify(self, Com, proofs):        
         assert(proofs["Cp"]["_Com"] == ((self.g ** proofs["stidle"]) * (self.h ** proofs["wtidle"])) * (Com["Com"] ** proofs["c"]))
             
-        for i in range(1, N+1):
+        for i in range(1, self.N+1):
             assert(proofs["Cp"]["_C1"][i] == (self.pks[i] ** proofs["pitidle"][i]) * (Com["C1"][i] ** proofs["c"]))
             
         wtidle = proofs['wtidle']        
-        indexArr = [i for i in range(1, N+1)]
+        indexArr = [i for i in range(1, self.N+1)]
         y = self.util.recoverCoefficients(indexArr)
         z = 0
         for i in indexArr:
@@ -103,10 +110,11 @@ class PVSS():
 
 
 if __name__ == "__main__":
-    groupObj = PairingGroup(setting.curveName)
-    
-    pvss = PVSS(groupObj)
-    print("N=%d,t=%d" % (N, t))
+
+    pvss = PVSS(groupObj, 22, 7)
+    sks={i: random_scalar() for i in range(0, pvss.N+1)}
+    [pvss.setPK(i, pvss.h ** sks[i]) for i in range(1, pvss.N+1)]
+    print("N=%d,t=%d" % (pvss.N, pvss.t))
     dist = pvss.share()
     # print("dis message size:",len(str(trans)))
     #print(dist["proof_sw"])
@@ -117,14 +125,14 @@ if __name__ == "__main__":
         print("the verify is false")
         exit(0)
 
-    T=[i for i in range(1, N+1)]
+    T=[i for i in range(1, pvss.N+1)]
     random.shuffle(T)
-    T = T[:t]
+    T = T[:pvss.t]
     # T=[i for i in range(1, t+1)]
 
     cis={}
     for i in T:
-        cis[i] = pvss.preRecon(dist["C"], pvss.sks[i],i)
+        cis[i] = pvss.preRecon(dist["C"], sks[i],i)
     
     testgstidle = pvss.recon(dist["C"], cis)
     print(testgstidle)
--- pvss/setting.py
+++
@@ -1,4 +0,0 @@
-N=100
-t=int(N/2)+1
-curveName="SS512"#
-# curveName="MNT159"
